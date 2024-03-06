619a064e84ec71a855d6731dca07163411d732a0
Anil Sai <anilkumar.sai333@gmail.com>
2024-03-06 21:13:14 +0530

    title added

--- src/class_work/CRUD/From.js
+++ src/class_work/CRUD/From.js
@@ -1,39 +1,72 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const From = (props) => {
-  const [product,setProducts]=useState(props.data)
-  let changeFormData=(event)=>{
-    const {name,value} = event.target;
-    setProducts({...product,[name]:value})
-  }
+  const [product, setProducts] = useState(props.data);
+  let changeFormData = (event) => {
+    const { name, value } = event.target;
+    setProducts({ ...product, [name]: value });
+  };
   return (
-    <div className='form-overlay'>
+    <div className="form-overlay">
       <form>
-        <div className='form-group'>
+        <h1>CRUD operation</h1>
+        <div className="form-group">
           <label>Name:</label>
-          <input  className='form-control mt-2' value={product.name} type='text' name='name' placeholder='Enter product Name' onChange={changeFormData}/>
+          <input
+            className="form-control mt-2"
+            value={product.name}
+            type="text"
+            name="name"
+            placeholder="Enter product Name"
+            onChange={changeFormData}
+          />
         </div>
-        <div className='form-group'>
+        <div className="form-group">
           <label>Price:</label>
-          <input className='form-control mt-2'value={product.price} type='number' name='price' placeholder='Enter Price' onChange={changeFormData}/>
+          <input
+            className="form-control mt-2"
+            value={product.price}
+            type="number"
+            name="price"
+            placeholder="Enter Price"
+            onChange={changeFormData}
+          />
         </div>
-        <div className='form-group'>
+        <div className="form-group">
           <label>Categery:</label>
-          <select className='form-control mt-2' value={product.categery} name='categery'onChange={changeFormData}>
-            <option value='-1'></option>
-            <option value='mobiles'>Mobiles</option>
-            <option value='laptops'>Laptops</option>
-            <option value='tv'>TV's</option>
-           </select>   
+          <select
+            className="form-control mt-2"
+            value={product.categery}
+            name="categery"
+            onChange={changeFormData}
+          >
+            <option value="-1"></option>
+            <option value="mobiles">Mobiles</option>
+            <option value="laptops">Laptops</option>
+            <option value="tv">TV's</option>
+          </select>
         </div>
-        <button className='btn btn-primary float-end' onClick={(e)=>{e.preventDefault(); props.add(product)}}>Send</button>
-        <button className='btn btn-danger float-end 'onClick={(e)=>{e.preventDefault()
-           props.close()} }>Cancel</button>
-
+        <button
+          className="btn btn-primary float-end"
+          onClick={(e) => {
+            e.preventDefault();
+            props.add(product);
+          }}
+        >
+          Send
+        </button>
+        <button
+          className="btn btn-danger float-end "
+          onClick={(e) => {
+            e.preventDefault();
+            props.close();
+          }}
+        >
+          Cancel
+        </button>
       </form>
     </div>
   );
-}
+};
 
 export default From;
-
